10262447661fe3f23de976d3ad31ebb86dbd551b
Jason Francois <jason.francois@Jasons-MacBook-Pro.local>
2021-09-03 21:34:49 -0400

    Set up initial React application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <div>
+        IP Address Tracker Search for any IP address or domain IP Address
+        Location Timezone UTC ISP
+      </div>
+      <div class="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
         </a>
-      </header>
+        . Coded by <a href="#">Your Name Here</a>.
+      </div>
     </div>
   );
 }
